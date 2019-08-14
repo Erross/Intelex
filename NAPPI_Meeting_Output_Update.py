7a19c647482763dd8e94fbeb9c1f552bbf0b114e
EWAN ROSS <erross@monsanto.com>
2019-08-14 10:17:31 -0500

    a

--- NAPPI_Meeting_Output_Update.py
+++ NAPPI_Meeting_Output_Update.py
@@ -1,8 +1,6 @@
-from requests import get
-from requests import post
+from requests import get, post, patch
 import urllib
 import json
-from requests import patch
 ### This whole thing is a sequential set of calls to move a complaint along and close it for being non supply chain for NAPPI workflow - it needs to be functionified and made pretty)
 #Credentials
 username ='$(GIMSUSERNAME)'
@@ -62,27 +60,6 @@ def ILXDirectChange(objectName,systemName,stringInput):
     except:
         return('Failed to change '+ objectname)
 
-###This whole thing needs to write
-#Validity
-#Investigation Details
-#Function
-#Process
-#Complaintcategory
-#ComplaintSub1
-#ComplaintSub2
-#Country
-#ResponsibleSite
-#Complaint coordinator location
-#Create CAR? (may need whole workflow for this...
-#Close Complaint
-#Response to Init
-#get object ID for the RECORD
-
- #Change Validity
-
-
-
-
 try:
     url = GIMSENVIRONMENT+'api/v2/object/' + objectname + "?$filter=RecordNumber eq "+ recordNumber
     response = get(url, auth=MyAuth)
@@ -95,6 +72,13 @@ try:
     objId = fullid_split[1].split(")")[0] #split string part the second
 except:
     print('failed to get an objectid')
+
+#CODE TO PUT INTO PILOT onProcess()
+context= pilotpython.Context(ctxt)
+data= pilotpython.DataRecord(dr)
+props = data.getProperties()
+
+
 #Get available action status for save and Submit Complaint - SETS RECORD UP FOR USE AND MAKES CLOSING AN OPTION IF NEED BE
 try:
     url = GIMSENVIRONMENT+'api/v2/object/'+objectname+'('+objId+')/Workflow/CurrentStage/Actions'
@@ -109,60 +93,92 @@ except:
 try:
     url = GIMSENVIRONMENT+'api/v2/object/'+objectname+'('+objId+')/Workflow/CurrentStage/Actions('+odataID+')/Action.ExecuteStageAction'
     response = post(url, auth=MyAuth)
+    props.defineStringProperty('Investigation_Status','Saved and Started')
 except:
     print('failed to save and instigate investigation')
+
+
 #VALIDITY
-ILXChange('MonCusComplaint_MonValidityObject', 'Caption', '$(Validity)', 'MonCusComplaint_CustomComplaintObject', 'Validity') #verified in test
-#ILXChange(referenceObject, referencePropertyName, referencePropertyValue, objectName, systemName)
-ILXChange('SyConfiguration_MonsantFunctionObject', 'ImportKey', '$(FunctionKey)', 'MonCusComplaint_CustomComplaintObject', 'Function') #verified in test
+if len('$(Validity')>0:
+    ILXChange('MonCusComplaint_MonValidityObject', 'Caption', '$(Validity)', 'MonCusComplaint_CustomComplaintObject', 'Validity')
+    props.defineStringProperty('Validity', '$(Validity)')
+
+
+if len('$(FunctionKey')>0:
+    ILXChange('SyConfiguration_MonsantFunctionObject', 'ImportKey', '$(FunctionKey)', 'MonCusComplaint_CustomComplaintObject', 'Function')
+    props.defineStringProperty('FunctionKey', '$(FunctionKey)')
 #Change Process
-ILXChange('SyConfiguration_MonsantoProcessObject', 'ImportKey', '$(ProcessKey)', 'MonCusComplaint_CustomComplaintObject', 'Process') #verified in test
-#Change Complaint Category
-ILXChange('MonCusComplaint_ComplaiCategoryObject', 'Value', '$(ComplaintCategory)', 'MonCusComplaint_CustomComplaintObject', 'ComplaiCategory') #verified in test - must be the code ie PERF_ENV
-#Change CS1
-ILXChange('MonCusComplaint_MonCatSubCat1Object', 'ComSubCategory1', '$(CSS1)', 'MonCusComplaint_CustomComplaintObject', 'ComSubCategory1') #verified in test - not code but full thing (may need to change above to match...)
-#Change CS2
-ILXChange('MonCusComplaint_MonCompSubCat12Object', 'ComSubCategory2', '$(CSS2)', 'MonCusComplaint_CustomComplaintObject', 'ComSubCategory2') #verified in test
-#Change Country
-#NEED TO ADD COUNTRYT TO LIST - DONT HAVE!!
-#Change Responsible Site
-ILXChange('SysLocationEntity', 'LocationCode', '$(Responsible Site)', 'MonCusComplaint_CustomComplaintObject', 'ResponsibleSite') #verified in test
-#change CCL
-ILXChange('SysLocationEntity', 'LocationCode', '$(CCL)', 'MonCusComplaint_CustomComplaintObject', 'CoordinLocation') #verified in test
+if len('$(ProcessKey)')>0:
+    ILXChange('SyConfiguration_MonsantoProcessObject', 'ImportKey', '$(ProcessKey)', 'MonCusComplaint_CustomComplaintObject', 'Process') #verified in test
+    props.defineStringProperty('ProcessKey', '$(ProcessKey)')
+
+if len('$(ComplaintCategory)')>0:
+    ILXChange('MonCusComplaint_ComplaiCategoryObject', 'Value', '$(ComplaintCategory)', 'MonCusComplaint_CustomComplaintObject', 'ComplaiCategory') #verified in test - must be the code ie PERF_ENV
+    props.defineStringProperty('ComplaintCategory', '$(ComplaintCategory)')
+
+if len('$(CSS1)')>0:
+    ILXChange('MonCusComplaint_MonCatSubCat1Object', 'SuCategory1Code', '$(CSS1)', 'MonCusComplaint_CustomComplaintObject', 'ComSubCategory1') #verified in test - not code but full thing (may need to change above to match...)
+    props.defineStringProperty('CSS1', '$(CSS1)')
+
+if len('$(CSS2)')>0:
+    ILXChange('MonCusComplaint_MonCompSubCat12Object', 'SuCategory2Code', '$(CSS2)', 'MonCusComplaint_CustomComplaintObject', 'ComSubCategory2') #verified in test
+    props.defineStringProperty('CSS2', '$(CSS2)')
+
+if len('$(ResponsibleSite')>0:
+    ILXChange('SysLocationEntity', 'LocationCode', '$(ResponsibleSite)', 'MonCusComplaint_CustomComplaintObject', 'ResponsibleSite') #verified in test
+    props.defineStringProperty('Responsible Site', '$(ResponsibleSite)')
+
+
+
+if len('$(CCL)') >0:
+    ILXChange('SysLocationEntity', 'LocationCode', '$(CCL)', 'MonCusComplaint_CustomComplaintObject', 'CoordinLocation') #verified in test
+    props.defineStringProperty('CCL', '$(CCL)')
 #change country
-ILXChange('MonAudit_MonsantoCountryObject', 'Caption', '$(Country)', 'MonCusComplaint_CustomComplaintObject', 'CCCountry')
+if len('$(Country')>0:
+    ILXChange('MonAudit_MonsantoCountryObject', 'Caption', '$(Country)', 'MonCusComplaint_CustomComplaintObject', 'CCCountry')
+    props.defineStringProperty('Country', '$(Country)')
 #change Response to initiator
 #THIS ONE CANT USE ILXCHANGE AS IT IS A DIRECT EDIT AND NOT THE ODATA DEPENDENT
 
-ILXDirectChange('MonCusComplaint_CustomComplaintObject','RetoInitiatedBy','$(ResponseToInitiatedBy)') #verified in test
+if len('$(ResponseToInitiatedBy')>0:
+    ILXDirectChange('MonCusComplaint_CustomComplaintObject','RetoInitiatedBy','$(ResponseToInitiatedBy)') #verified in test
+    props.defineStringProperty('Response to Initiated By', '$(ResponseToInitiatedBy)')
 
 #change Investigation Detals
 
-ILXDirectChange('MonCusComplaint_CustomComplaintObject','InvestigDetails','$(InvestigationDetails)') #verified in test
+if len('$(InvestigationDetails')>0:
+    ILXDirectChange('MonCusComplaint_CustomComplaintObject','InvestigDetails','$(InvestigationDetails)') #verified in test
+    props.defineStringProperty('InvestigationDetails', '$(InvestigationDetails)')
+#CREATE A CAR??!
 
+if len('$(CreateaCAR')>0:
+    ILXDirectChange('MonCusComplaint_CustomComplaintObject', 'CreateaCAR', '$(CreateCAR)') #verified in testneed to add a workflow here that automates CAR creation if createacar = TRUE (different script most likely)
+    props.defineStringProperty('CreateACAR', '$(CreataCAR)')
 
-
-#Close Request
+#Close Request ONLY DO THIS IF CLOSE = TRUE
 #Get workflow id
 
+if '$(CloseComplaint)' == 'TRUE':
+    try:
+        url = GIMSENVIRONMENT+'api/v2/object/'+objectname+'('+objId+')/Workflow/CurrentStage/Actions'
+        response = get(url, auth=MyAuth)
+        x = response.json()
 
-try:
-    url = GIMSENVIRONMENT+'api/v2/object/'+objectname+'('+objId+')/Workflow/CurrentStage/Actions'
-    response = get(url, auth=MyAuth)
-    x = response.json()
-
-    for y in x['value']:
-        if y['Name'] == 'Complete Complaint':
-            odataID2 = y['Id']
+        for y in x['value']:
+            if y['Name'] == 'Complete Complaint':
+                odataID2 = y['Id']
 
 
-    print("odataID2 "+odataID2)
-except:
-    print('failed to get odataID2')
+        print("odataID2 "+odataID2)
 
-try:
-    url = GIMSENVIRONMENT+'api/v2/object/'+objectname+'('+objId+')/Workflow/CurrentStage/Actions('+odataID2+')/Action.ExecuteStageAction'
+    except:
+        print('failed to get odataID2')
 
-    response = post(url, auth=MyAuth)
-except:
-    print('failed to close investigation')
+    try:
+        url = GIMSENVIRONMENT+'api/v2/object/'+objectname+'('+objId+')/Workflow/CurrentStage/Actions('+odataID2+')/Action.ExecuteStageAction'
+
+        response = post(url, auth=MyAuth)
+        props.defineStringProperty('InvestigationClosed', '$(CloseComplaint)')
+    except:
+        print('failed to close investigation')
+        props.defineStringProperty('InvestigationClosed', 'failed to close investigation')
